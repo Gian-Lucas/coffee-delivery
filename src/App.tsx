fda59e6b09907b6434ee30dbcffb331eb5f41f06
Gian Lucas <lgian584@gmail.com>
2022-08-06 17:48:30 -0300

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <div>
+      <h1 className="text-base-title text-4xl">
+        Hello World - Coffee Delivery
+      </h1>
+    </div>
+  );
+}
