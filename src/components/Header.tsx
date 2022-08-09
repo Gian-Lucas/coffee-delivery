3d5774b683eba34f6f06182e3d42e060ef7fbe66
Gian Lucas <lgian584@gmail.com>
2022-08-09 14:29:33 -0300

    header cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,8 +12,13 @@ export function Header() {
           <span>Porto Alegre, RS</span>
         </div>
 
-        <div className="flex items-center bg-yellow-light rounded-lg p-2">
+        <div className="flex items-center relative bg-yellow-light rounded-lg p-2">
           <ShoppingCart size={22} weight="fill" className="text-yellow-dark" />
+          {true && (
+            <span className="flex items-center -right-2 -top-2 absolute w-5 h-5 justify-center text-white font-bold text-xs bg-yellow-dark rounded-full">
+              3
+            </span>
+          )}
         </div>
       </div>
     </header>
