6eaaf99b48fddcf68fe5fceda9f8355e3ab662fe
Gian Lucas <lgian584@gmail.com>
2022-08-11 20:43:33 -0300

    card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,61 @@
+import { Minus, Plus, ShoppingCart } from "phosphor-react";
+import coffee from "../assets/coffee.svg";
+
+export function Card() {
+  return (
+    <div className="w-64 h-[19.375rem] bg-base-card text-center rounded-tl-md rounded-br-md rounded-tr-[2.25rem] rounded-bl-[2.25rem]">
+      <img src={coffee} alt="" className="mx-auto -mt-5" />
+      <div className="mt-3 ">
+        <span
+          className="py-1 px-2 bg-yellow-light text-yellow-dark rounded-[100px] 
+        font-bold text-[.625rem]"
+        >
+          TRADICIONAL
+        </span>
+      </div>
+
+      <h1 className="mt-4 font-header text-xl text-base-subtitle">
+        Expresso Tradicional
+      </h1>
+
+      <p className="mt-2 mx-5 text-base-label text-sm">
+        O tradicional café feito com água quente e grãos moídos
+      </p>
+
+      <div className="mt-8 mx-6 mb-5 flex items-center justify-between">
+        <div className="text-base-text">
+          <span className="text-sm">R$ </span>
+          <strong className="font-extrabold font-header text-2xl leading-8">
+            9,90
+          </strong>
+        </div>
+        <div className="flex items-center gap-2">
+          <div
+            className="flex justify-center items-center text-purple bg-base-button         
+          p-2 gap-2 rounded-md"
+          >
+            <Minus
+              className="transition-colors hover:text-purple-dark cursor-pointer"
+              size={14}
+              weight="bold"
+            />
+            <span className="text-base-title">1</span>
+            <Plus
+              className="transition-colors hover:text-purple-dark cursor-pointer"
+              size={14}
+              weight="bold"
+            />
+          </div>
+          <div>
+            <button
+              className="transition-colors bg-purple-dark hover:bg-purple p-2 rounded-md text-base-card"
+              title="Mostrar carrinho"
+            >
+              <ShoppingCart size={22} weight="fill" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,6 @@
-import {
-  Coffee,
-  Minus,
-  Package,
-  Plus,
-  ShoppingCart,
-  Timer,
-} from "phosphor-react";
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
 import cupOfCoffee from "../assets/cup-coffee.svg";
-import coffee from "../assets/coffee.svg";
+import { Card } from "../components/Card";
 import { ItemWithText } from "../components/ItemWithText";
 
 export function Home() {
@@ -60,55 +53,14 @@ export function Home() {
           Nossos cafés
         </h1>
 
-        <div className="my-14 flex flex-wrap gap-x-8 gap-y-10">
-          <div className="w-64 h-[19.375rem] bg-base-card text-center rounded-tl-md rounded-br-md rounded-tr-[2.25rem] rounded-bl-[2.25rem]">
-            <img src={coffee} alt="" className="mx-auto -mt-5" />
-            <div className="mt-3 ">
-              <span className="py-1 px-2 bg-yellow-light text-yellow-dark rounded-[100px] font-bold text-[.625rem]">
-                TRADICIONAL
-              </span>
-            </div>
-
-            <h1 className="mt-4 font-header text-xl text-base-subtitle">
-              Expresso Tradicional
-            </h1>
-
-            <p className="mt-2 mx-5 text-base-label text-sm">
-              O tradicional café feito com água quente e grãos moídos
-            </p>
-
-            <div className="mt-8 mx-6 mb-5 flex items-center justify-between">
-              <div className="text-base-text">
-                <span className="text-sm">R$ </span>
-                <strong className="font-extrabold font-header text-2xl leading-8">
-                  9,90
-                </strong>
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="flex justify-center items-center text-purple bg-base-button         p-2 gap-2 rounded-md">
-                  <Minus
-                    className="transition-colors hover:text-purple-dark cursor-pointer"
-                    size={14}
-                    weight="bold"
-                  />
-                  <span className="text-base-title">1</span>
-                  <Plus
-                    className="transition-colors hover:text-purple-dark cursor-pointer"
-                    size={14}
-                    weight="bold"
-                  />
-                </div>
-                <div>
-                  <button
-                    className="transition-colors bg-purple-dark hover:bg-purple p-2 rounded-md text-base-card"
-                    title="Mostrar carrinho"
-                  >
-                    <ShoppingCart size={22} weight="fill" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="my-14 grid grid-cols-4 gap-x-8 gap-y-10">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </main>
     </div>
