660c6e2c5f9d5e78eb9be3a4c5369106c9af0ba7
Gian Lucas <lgian584@gmail.com>
2022-08-11 20:15:18 -0300

    bg item with text component

--- src/components/ItemWithText.tsx
+++ src/components/ItemWithText.tsx
@@ -3,16 +3,13 @@ import { ReactNode } from "react";
 interface ItemWithTextProps {
   text: string;
   bg: string;
-
   icon: ReactNode;
 }
 
 export function ItemWithText({ icon, text, bg }: ItemWithTextProps) {
   return (
     <div className="flex text-sm items-center gap-3">
-      <div
-        className={`p-2 bg-base-text text-background rounded-full w-fit ${bg}`}
-      >
+      <div className={`p-2 text-background rounded-full w-fit ${bg}`}>
         {icon}
       </div>
       <span>{text}</span>
