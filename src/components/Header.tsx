401052bee6d1fa4ac01ac44649a7be21d352b299
Gian Lucas <lgian584@gmail.com>
2022-08-12 13:02:45 -0300

    header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { MapPin, ShoppingCart } from "phosphor-react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import { CartHeader } from "./CartHeader";
 
 export function Header() {
   return (
     <header className="flex justify-between items-center max-w-[1120px] mx-auto py-8 px-10">
-      <img src={logo} alt="Coffee Delivery Logo" />
+      <Link to="/">
+        <img src={logo} alt="Coffee Delivery Logo" />
+      </Link>
 
       <div className="flex justify-end items-center gap-3">
         <div className="flex items-center gap-1 text-sm bg-purple-light text-purple-dark p-2 rounded-lg">
